8745a5d5b928de90218f5d694ca44a695eb10d06
leoor22 <leonelrivas.rl@gmail.com>
2022-04-15 16:47:32 -0300

    app finished

---
+++ src/types/types.js
@@ -0,0 +1,23 @@
+export const types = {
+  uiOpenModal: "[UI] Open modal",
+  uiCloseModal: "[UI] Close modal",
+  //
+  //
+  eventSetActive: "[Event] Set Active",
+  eventLogout: "[Event] Store cleaned",
+
+  eventStartNew: "[Event] Start add new",
+  eventAddNew: "[Event] Add new",
+  eventClearActiveEvent: "[Event] Clear active event",
+  eventUpdated: "[Event] Event updated",
+  eventDeleted: "[Event] Event deleted",
+  eventLoaded: "[Event] Events loaded",
+  //
+  //
+  authCheckingFinish: "[Auth] Finish checking login state",
+  authStartLogin: "[Auth] Start login",
+  authLogin: "[Auth] Login",
+  authStartRegister: "[Auth] Start register",
+  authStartTokenRenew: "[Auth] Start token renew",
+  authLogout: "[Auth] logout",
+};
